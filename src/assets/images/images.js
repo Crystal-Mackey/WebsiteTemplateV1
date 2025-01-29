83f54a285069fb67817e56da07f297ca93a8012b
Crystal Mackey <moonrunner575@gmail.com>
2025-01-28 20:39:33 -0500

    filler gallery images

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,8 +1,102 @@
 import DesignLogo from "../icons/MaxArtistryDesigns2.jpg";
-
+import gallery1 from "./gallery1.png";
+import gallery2 from "./gallery2.png";
+import gallery3 from "./gallery3.png";
+import gallery4 from "./gallery4.png";
+import gallery5 from "./gallery5.png";
+import gallery6 from "./gallery6.png";
+import gallery7 from "./gallery7.png";
+import gallery8 from "./gallery8.png";
+import gallery9 from "./gallery9.png";
+import gallery10 from "./gallery10.png";
+import gallery11 from "./gallery11.png";
+import gallery12 from "./gallery12.png";
+import gallery13 from "./gallery13.png";
+import gallery14 from "./gallery14.png";
+import gallery15 from "./gallery15.png";
+import gallery16 from "./gallery16.png";
+import gallery17 from "./gallery17.png";
+import gallery18 from "./gallery18.png";
+import gallery19 from "./gallery19.png";
 const images = [
     {
         src:DesignLogo,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery1,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery2,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery3,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery4,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery5,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery6,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery7,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery8,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery9,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery10,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery11,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery12,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery13,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery14,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery15,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery16,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery17,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery18,
+        alt: 'filler image for gallery template'
+    },
+    {
+        src:gallery19,
         alt: 'filler image for gallery template'
     },
 ];
